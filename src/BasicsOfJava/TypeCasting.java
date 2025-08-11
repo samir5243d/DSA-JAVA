45a349bfdd8e7636a8f69c88072973d59fe9f169
samir5243d <samir5243d@gmail.com>
2025-08-11 22:06:05 +0530

    added new stuff

---
+++ src/BasicsOfJava/TypeCasting.java
@@ -0,0 +1,21 @@
+package BasicsOfJava;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+
+//
+//       float a= 25.12f;
+////       int b = a; //possible lossy conversion from float to int
+//
+//        int b= (int)a; // forcefully conversion with knowing there will be loss of data
+//        System.out.println(b);
+//
+
+        char ch  = 'a';
+        char ch2  = 'b';
+        int number = ch;
+        int number2 = ch2;
+        System.out.println(number);
+        System.out.println(number2);
+   }
+}
---
+++ src/BasicsOfJava/TypePromotionInExpression.java
@@ -0,0 +1,14 @@
+package BasicsOfJava;
+
+public class TypePromotionInExpression {
+    public static void main(String[] args) {
+
+        char a= 'a';
+        char b= 'b';
+        System.out.println((int) (b));
+        System.out.println((int) (a));
+        System.out.println(b-a);
+
+
+    }
+}
