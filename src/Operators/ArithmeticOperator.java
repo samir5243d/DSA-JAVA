d1d50541c5184c7b76fc4f8b7679480537e37551
samir5243d <samir5243d@gmail.com>
2025-08-13 22:05:23 +0530

    started learning operators

---
+++ src/Operators/ArithmeticOperator.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=5;
+
+        System.out.println("add = "+ (a+b));
+        System.out.println("subtract = "+ (a-b));
+        System.out.println("multiply = "+ (a*b));
+        System.out.println("divide = "+ (a/b));
+        System.out.println("modulo = "+ (a%b)); // it gives remainder
+
+
+
+    }
+}
