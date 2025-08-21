f1bebae373d8d74c724f5501df4294b353326e12
samir5243d <samir5243d@gmail.com>
2025-08-21 23:24:19 +0530

    addded new challenge

---
+++ src/ConditionalStatements/Calculator.java
@@ -0,0 +1,41 @@
+package ConditionalStatements;
+
+
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+
+        char operator=sc.next().charAt(0);
+
+        switch (operator){
+            case '+' : System.out.println(a+b);
+
+                     break;
+
+            case '-'  : System.out.println(a-b);
+
+                     break;
+
+            case '*'  : System.out.println(a*b);
+                     break;
+
+            case '/' : System.out.println(a/b);
+
+                      break;
+
+            case '%' : System.out.println(a%b);
+
+                      break;
+
+            default : System.out.println("calculator is not that of advance");
+        }
+
+
+
+    }
+}
