711a8ee35fd302cba04bf90bdd731d5455874c09
samir5243d <samir5243d@gmail.com>
2025-08-25 22:19:06 +0530

    added nw challenges

---
+++ src/LoopsFlowControl/Q1.java
@@ -0,0 +1,18 @@
+package LoopsFlowControl;
+
+//PRINT NUMBER FROM 1 TO 10;
+
+
+public class Q1 {
+    public static void main(String[] args) {
+
+        int counter =1;
+
+        while (counter <=10){
+            System.out.println(counter);
+            counter++;
+        }
+
+
+    }
+}
