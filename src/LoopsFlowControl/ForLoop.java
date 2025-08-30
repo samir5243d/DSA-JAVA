8c84712f49134fb4bc1275a3efafc9699fb3b36c
samir5243d <samir5243d@gmail.com>
2025-08-30 22:15:01 +0530

    solved new type of question in loops

---
+++ src/LoopsFlowControl/ForLoop.java
@@ -0,0 +1,12 @@
+package LoopsFlowControl;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+//        int i=1;
+
+        for (int i=1; i<=10; i++){
+            System.out.println("hello world");
+        }
+    }
+}
---
+++ src/LoopsFlowControl/Q4.java
@@ -0,0 +1,13 @@
+package LoopsFlowControl;
+
+//PRINT SQUARE PATTERN
+
+public class Q4 {
+    public static void main(String[] args) {
+
+        for (int line=1; line<=4; line++){
+
+            System.out.println("****");
+        }
+    }
+}
