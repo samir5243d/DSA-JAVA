eb99f5d5d37c51b691ef45d9b5ebde4f8e41fd67
samir5243d <samir5243d@gmail.com>
2025-09-21 22:31:06 +0530

    added new stuff after very interval of time

---
+++ src/LoopsFlowControl/DoWhileLOOp.java
@@ -0,0 +1,12 @@
+package LoopsFlowControl;
+
+public class DoWhileLOOp {
+    public static void main(String[] args) {
+
+        int counter=1;
+        do {
+            System.out.println("hello bachho");
+            counter++;
+        } while(counter <=10);
+    }
+}
