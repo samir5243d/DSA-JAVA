d5cf52063f68f54cbaab164ff0ee3465ce43c595
samir5243d <samir5243d@gmail.com>
2025-10-05 01:06:44 +0530

    addedd new stuff but it is not clearly right

--- src/PATTERNS/Q1printStarPttern.java
+++ src/PATTERNS/Q1printStarPttern.java
@@ -1,4 +1,16 @@
 package PATTERNS;
 
 public class Q1printStarPttern {
+    public static void main(String[] args) {
+
+        for (int line=1; line<=4; line++){
+            for (int star =1; star<=line; star++ ){
+                System.out.println("*");
+            }
+            System.out.println();
+
+        }
+    }
+
+
 }
