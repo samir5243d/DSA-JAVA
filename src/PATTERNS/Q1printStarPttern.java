6ad5459c25187689d34e505d2ecb91424b00e059
samir5243d <samir5243d@gmail.com>
2025-10-13 23:06:05 +0530

    added new stuff

--- src/PATTERNS/Q1printStarPttern.java
+++ src/PATTERNS/Q1printStarPttern.java
@@ -3,16 +3,18 @@ package PATTERNS;
 public class Q1printStarPttern {
     public static void main(String[] args) {
 
-//        for (int line=1; line<=4; line++){
-//            for (int star =1; star<=line; star++ ){
-//                System.out.print("*");
-//            }
-//            System.out.println();
-        for (int line =1;  line<=4; line++);
+        for (int line=1; line<=4; line++){
+            for (int star =1; star<=line; star++ ){
+                System.out.println("*");
+            }
+
+            }
+
 
 
         }
-    }
+
+
 
 
 
--- src/PATTERNS/Q2.java
+++ src/PATTERNS/Q2.java
@@ -3,4 +3,15 @@ package PATTERNS;
 
 //print inverted star pattern
 public class Q2 {
+    public static void main(String[] args) {
+
+        int n=4;
+        for (int line=1; line<=4; line++){
+            for(int star =1; star<=(n-line+1); star++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
 }
