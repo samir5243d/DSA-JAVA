299dbcb4b66141fbd25a7f4e85949bb3dbe3562c
samir5243d <samir5243d@gmail.com>
2025-10-16 00:31:45 +0530

    added new stuff

---
+++ src/PATTERNS/Q3.java
@@ -0,0 +1,23 @@
+package PATTERNS;
+
+//HALF -PYRAMID PATTERN
+
+public class Q3 {
+    public static void main(String[] args) {
+
+          int n=4;
+
+        for (int line =1; line<=n; line++){
+            //numbers print
+            for (int number =1; number<=line; number++){
+
+                System.out.print(number);
+            }
+            System.out.println();
+
+        }
+
+    }
+
+
+}
