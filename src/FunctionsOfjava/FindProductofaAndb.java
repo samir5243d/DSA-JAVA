c530a0a6174ab94caa91aac69ceae2081f31e397
samir5243d <samir5243d@gmail.com>
2025-11-10 22:11:50 +0530

    added new varients and modified existing codes

---
+++ src/FunctionsOfjava/FindProductofaAndb.java
@@ -0,0 +1,32 @@
+package FunctionsOfjava;
+
+public class FindProductofaAndb {
+
+
+//    public static void swap(int a, int b){
+//        int temp =a;
+//        a=b;
+//        b= temp;
+//    }
+
+    public static int multiply(int a, int b){
+        int product = a*b;
+        return product;
+    }
+
+    public static void main(String[] args) {
+        int a=3;
+        int b=5;
+       int prod = multiply(a,b);
+
+        System.out.println("a*b = " + prod);
+
+        prod = multiply(10,20);
+
+        System.out.println(" a*b= " + prod);
+
+
+
+
+    }
+}
---
+++ src/PATTERNS/Q4.java
@@ -0,0 +1,22 @@
+
+package PATTERNS;
+
+//print character pattern.java;
+
+public class Q4 {
+    public static void main(String[] args) {
+        int n = 6;
+        char ch = 'A';
+
+        for (int line = 1; line <= n; line++) {
+            for (int chars = 1; chars <= line; chars++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
