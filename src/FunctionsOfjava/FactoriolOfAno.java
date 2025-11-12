0de99eebcaab433a0a26e69a5b3797890fa8bf3a
samir5243d <samir5243d@gmail.com>
2025-11-13 00:58:39 +0530

    modified codes of factoriol

---
+++ src/FunctionsOfjava/FactoriolOfAno.java
@@ -0,0 +1,19 @@
+package FunctionsOfjava;
+
+public class FactoriolOfAno {
+
+    public static int factoriol(int n){
+        int f=1;
+
+        for (int i=1; i<=n; i++){
+
+            f= f* i;
+
+        }
+        return f;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
