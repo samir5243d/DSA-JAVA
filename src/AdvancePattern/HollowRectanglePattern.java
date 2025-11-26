14022240c81f9df03d389799c224b9fae00b3b33
samir5243d <samir5243d@gmail.com>
2025-11-26 09:20:20 +0530

    started learning advanced pattern of java

---
+++ src/AdvancePattern/HollowRectanglePattern.java
@@ -0,0 +1,38 @@
+package AdvancePattern;
+
+import javax.swing.*;
+
+public class HollowRectanglePattern {
+
+
+    public static  void hollow_rectangle(int totRows, int totCols){
+
+        for (int i =1; i<=totRows; i++){
+
+            // inner- columns
+
+            for (int j=1; j<=totCols; j++){
+
+                // cell- (i,j)
+
+                if (i==1 || i== totRows || j==1 || j== totCols){
+                    // boundry
+
+                    System.out.print("*");
+
+                }else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        hollow_rectangle(4,5);
+
+    }
+}
