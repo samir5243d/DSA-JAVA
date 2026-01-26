00dc2f7defad5105f410ef46a7acfacb42723c62
samir5243d <samir5243d@gmail.com>
2026-01-26 10:18:33 +0530

    modified existing stuff

--- src/ARRAY/PairsInarray.java
+++ src/ARRAY/PairsInarray.java
@@ -1,5 +1,35 @@
 package ARRAY;
 
+import java.util.Scanner;
+
 public class PairsInarray {
+
+    public static void PrintPairs(int numbers[]){
+
+        int tp = 0;
+
+        for (int i=0; i<numbers.length; i++){
+            int current = numbers[i]; //2, 4, 6, 8, 10
+
+            for (int j= i+1; j<numbers.length; j++){
+
+                System.out.print("(" + current + "," +numbers[j]+")");
+
+                tp++;
+
+            }
+            System.out.println();
+
+        }
+        System.out.println(" total pairs = "+ tp);
+    }
+
+    public static void main(String[] args) {
+
+      int   numbers[]= {2,4,6,8,10};
+
+      PrintPairs(numbers);
+
+    }
 
 }
